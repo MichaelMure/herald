f506fb5a50c8fd92e1c34390a3b229b19fea6db2
Michael Muré <batolettre@gmail.com>
2024-07-02 18:28:54 +0200

    comment example

--- example.go
+++ example.go
@@ -1,39 +1,33 @@
 package herald
 
-import (
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/ipni/go-libipni/announce/httpsender"
-)
-
 func Example() {
-	awsCfg := aws.NewConfig()
-
-	backend := NewS3Backend(awsCfg, "bucket", "/indexer/ingest/mainnet", keys)
-
-	announcer := httpsender.New()
-
-	batcher := StartCatalogBatcher(BatchConfig{
-		CountThreshold:         1000,
-		MaxDelay:               DefaultMaxDelay,
-		MaxMHsPerAdvertisement: DefaultMaxMHsPerAdvertisement,
-	}, ChainConfig{
-		AdEntriesChunkSize: 0,
-		PublisherId:        "",
-		PublisherKey:       nil,
-		ProviderAddrs:      nil,
-		PublisherHttpAddrs: nil,
-		Metadata:           nil,
-	}, backend, announcer)
-
-	for {
-		// read from SQS
-		msg := read()
-
-		if msg.Publish {
-			batcher.PublishCatalog(ctx, CatalogFromFoo(...))
-		} else {
-			batcher.RetractCatalog(ctx, CatalogFromFoo(...))
-		}
-	}
-
+	// awsCfg := aws.NewConfig()
+	//
+	// backend := NewS3Backend(awsCfg, "bucket", "/indexer/ingest/mainnet", keys)
+	//
+	// announcer := httpsender.New()
+	//
+	// batcher := StartCatalogBatcher(BatchConfig{
+	// 	CountThreshold:         1000,
+	// 	MaxDelay:               DefaultMaxDelay,
+	// 	MaxMHsPerAdvertisement: DefaultMaxMHsPerAdvertisement,
+	// }, ChainConfig{
+	// 	AdEntriesChunkSize: 0,
+	// 	PublisherId:        "",
+	// 	PublisherKey:       nil,
+	// 	ProviderAddrs:      nil,
+	// 	PublisherHttpAddrs: nil,
+	// 	Metadata:           nil,
+	// }, backend, announcer)
+	//
+	// for {
+	// 	// read from SQS
+	// 	msg := read()
+	//
+	// 	if msg.Publish {
+	// 		batcher.PublishCatalog(ctx, CatalogFromFoo(...))
+	// 	} else {
+	// 		batcher.RetractCatalog(ctx, CatalogFromFoo(...))
+	// 	}
+	// }
 }
